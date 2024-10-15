7b26d5a1d7484af4ee989b7fe19db63fb9c2b9cc
manhh <manh5815@gmail.com>
2024-10-15 14:04:46 +0700

    BTTH2

---
+++ BTTH2_Practice/practice_01/app/src/main/java/com/example/practice_01/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.practice_01;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import javax.security.auth.Subject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 1;
+    private TextView resultTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button startActivityButton = findViewById(R.id.startNewActivityButton);
+        resultTextView = findViewById(R.id.resultTextView);
+
+        startActivityButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Subject.class);
+            startActivityForResult(intent, REQUEST_CODE);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            String name = data.getStringExtra("name");
+            String gpa = data.getStringExtra("gpa");
+            resultTextView.setText("Họ và tên: " + name + "\nĐiểm GPA: " + gpa);
+        }
+    }
+}
